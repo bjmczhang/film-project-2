cbfd335db4f934d44155742d285350ea2b75ed51
AceDev <133585997+bjmczhang@users.noreply.github.com>
2023-07-18 15:34:01 +1000

    Update FilmLibrary.js

--- src/FilmLibrary.js
+++ src/FilmLibrary.js
@@ -102,20 +102,11 @@ function FilmLibrary() {
             value={selectedYear}
             onChange={handleYearChange}
             className="selectYear"
-          >
-            <option value="">All Years</option>
-            <option value="2023">2023</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-            <option value="2012">2012</option>
+          >{["all", ...Array.from({ length: 24 }, (_, i) => 2023 - i)].map(
+              (year) => (
+                <option value={year}>{year}</option>
+              )
+            )}
           </select>
         ) : null}
 
